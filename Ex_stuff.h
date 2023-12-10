2ebfe5cf192dc71f38dcb75e912c5ed3d154dd1d
Ludicrous_peridot <>
2023-12-10 00:00:00 +0000

    Fixing typos in Ex_stuff.h to re-enable JUMPWAD jumping

--- Ex_stuff.h
+++ Ex_stuff.h
@@ -61,7 +61,7 @@ extern char ** dyna_playpal_wads;    // names of the wad containing a PLAYPAL lu
 byte extra_status[NUM_EXTRAS];
 
 #define DOOMED_SELFIE (-1000)
-#define DOOMED_JUMP (-1000)
+#define DOOMED_JUMP (-2000)
 
 void Ex_ResetExtraStatus();
 
@@ -70,7 +70,7 @@ void Ex_ResetExtraStatus();
 
 #define EXTRA_STATES_TABLE(extra) ((extra) == EXTRA_SELFIE ? states3[1] : (extra) == EXTRA_JUMP ? states3[2] : states3[0])
 #define EXTRA_WEAPONS_TABLE(extra) ((extra) == EXTRA_SELFIE ? weaponinfo3[1] : (extra) == EXTRA_JUMP ? weaponinfo3[2] : weaponinfo3[0])
-#define EXTRA_ACTOR_STATES(actor) (((actor)->intflags & MIF_STATE2) ? states3[1] : ((actor)->intflags & MIF_STATE3) ? states3[1] : states3[0])
+#define EXTRA_ACTOR_STATES(actor) (((actor)->intflags & MIF_STATE2) ? states3[1] : ((actor)->intflags & MIF_STATE3) ? states3[2] : states3[0])
 #define EXTRA_PLAYER_STATES(player) (((player)->cheats & CF_SELFIE) ? states3[1] : ((player)->cheats & CF_JUMP) ? states3[2] : states3[0])
 #define EXTRA_PLAYER_WEAPONS(player) (((player)->cheats & CF_SELFIE) ? weaponinfo3[1] : ((player)->cheats & CF_JUMP) ? weaponinfo3[2] : weaponinfo3[0])
 #define EXTRA_INFO_STATES(info) ((info)->doomednum == DOOMED_SELFIE ? states3[1] : (info)->doomednum == DOOMED_JUMP ? states3[2] : states3[0]) 
